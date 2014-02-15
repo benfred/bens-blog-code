ef86363c57157862fc42b4658c31b8eac7daeff7
Seth Woodworth <seth@sethish.com>
2014-02-15 13:07:37 -0500

    save graphs to png rather than than pyplot.show \n compatible with more system types

--- python-serialization/chart.py
+++ python-serialization/chart.py
@@ -8,18 +8,19 @@ import json
 BAR_WIDTH = 0.35
 OPACITY = 0.4
 
+
 def speedChart(data):
     fig, ax = pyplot.subplots()
     index = numpy.arange(len(data))
 
-    rects1 = pyplot.bar(index, 
-                        [d['packRate'] for d in data], 
+    rects1 = pyplot.bar(index,
+                        [d['packRate'] for d in data],
                         BAR_WIDTH,
                         alpha=OPACITY,
                         color='b',
                         label='Pack')
 
-    rects2 = pyplot.bar(index + BAR_WIDTH, 
+    rects2 = pyplot.bar(index + BAR_WIDTH,
                         [d['unpackRate'] for d in data],
                         BAR_WIDTH,
                         alpha=OPACITY,
@@ -31,21 +32,24 @@ def speedChart(data):
     pyplot.legend(loc='upper left')
 
     pyplot.tight_layout()
-    pyplot.show()
+    #pyplot.show()
+    pyplot.savefig('speedchart.png')
+
 
 def sizeChart(data):
     fig, ax = pyplot.subplots()
     index = numpy.arange(len(data))
-    rects1 = pyplot.bar(index, 
-                        [d['averageSize'] for d in data], 
+    rects1 = pyplot.bar(index,
+                        [d['averageSize'] for d in data],
                         BAR_WIDTH,
                         alpha=OPACITY,
                         color='b')
 
     pyplot.ylabel('Average Size (bytes)')
-    pyplot.xticks(index + BAR_WIDTH/2, [d['method'] for d in data])
+    pyplot.xticks(index + BAR_WIDTH / 2, [d['method'] for d in data])
     pyplot.tight_layout()
-    pyplot.show()
+    #pyplot.show()
+    pyplot.savefig('sizechart.png')
 
 if __name__ == "__main__":
     data = json.load(open("speed_data.json"))
